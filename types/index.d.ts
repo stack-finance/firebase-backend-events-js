2fcf393c74963b67b189610e64590cb7b08308ac
eetrip <eeshantripathi111@gmail.com>
2022-07-25 17:44:02 +0530

    fix: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,6 +7,6 @@ export class FirebaseEvents {
      * @param {object} payload - An object which contains the JSON payload defined in the docs here: https://developers.moengage.com/hc/en-us/articles/4404674776724-Overview#request-body-0-5
      * @returns {Promise} A response promise(fetch promise) from the provider, if successful, the response will be in JSON format. Errors need to be handled separately.
      */
-     addOrUpdateUser(payload: object): Promise<any>
+     triggerEvent(payload: object): Promise<any>
   
 }
