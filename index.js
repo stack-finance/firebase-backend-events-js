957a2b5681f1a52ebe0ad64657a63acdacdcc6a0
eetrip <eeshantripathi111@gmail.com>
2022-08-10 15:05:03 +0530

    fix: generalised payload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export class FirebaseEvents {
   constructor(firebaseAppId, apiSecret) {
@@ -7,37 +7,14 @@ export class FirebaseEvents {
   }
 
   triggerEvent(payload) {
-    if (
-      !payload
-      || !payload?.app_instance_id
-      || !payload?.event?.name
-    ) {
-      console.log('failed to trigger firebase event, required feilds missing');
-      return {};
-    }
-    const {
-      app_instance_id,
-      non_personalized_ads = false,
-      event: {
-        name,
-        params
-      }
-    } = payload;
-    return fetch(`https://www.google-analytics.com/mp/collect?firebase_app_id=${this.firebaseAppId}&api_secret=${this.apiSecret}`, {
+    return fetch(
+      `https://www.google-analytics.com/mp/collect?firebase_app_id=${this.firebaseAppId}&api_secret=${this.apiSecret}`,
+      {
         method: "POST",
-        body: JSON.stringify({
-          "app_instance_id": app_instance_id,
-          "non_personalized_ads": non_personalized_ads,
-          "events": [
-            {
-              "name": name || '',
-              "params": params || {}
-            }
-          ]
-        })
-      });
+        body: JSON.stringify(payload),
+      }
+    );
   }
-
-};
+}
 
 export default { FirebaseEvents };
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,8 +3,8 @@ export class FirebaseEvents {
     constructor()
   
     /**
-     * Method to add a new user or update an existing user.
-     * @param {object} payload - An object which contains the JSON payload defined in the docs here: https://developers.moengage.com/hc/en-us/articles/4404674776724-Overview#request-body-0-5
+     * Method to add a new event or update an existing event.
+     * @param {object} payload - An object which contains the JSON payload defined in the docs here: https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=firebase#payload_post_body
      * @returns {Promise} A response promise(fetch promise) from the provider, if successful, the response will be in JSON format. Errors need to be handled separately.
      */
      triggerEvent(payload: object): Promise<any>
