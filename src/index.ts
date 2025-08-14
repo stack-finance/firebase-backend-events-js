f7a2a783b1e6ea2411919896ae9a53e43f2c55e3
Parikshit Rao <parikshit.rao@stackfinance.co>
2025-08-14 18:34:32 +0530

    fix: converted to typescript and added type dependancies

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,31 @@
 import fetch from "node-fetch";
+import type { Logger } from "pino";
 
 export class FirebaseEvents {
-  constructor(firebaseAppId, apiSecret) {
+  private readonly firebaseAppId: string;
+  private readonly apiSecret: string;
+  logger?: Logger;
+
+  constructor(firebaseAppId: string, apiSecret: string) {
     this.firebaseAppId = firebaseAppId;
     this.apiSecret = apiSecret;
-    this.logger = null;
   }
 
-  setLogger(logger) {
+  setLogger(logger: Logger) {
     this.logger = logger;
   }
 
-  triggerEvent(payload) {
-    if (this.logger === null) {
-      console.error("Logger has not been set! Defaulting to 'console.info'");
-      console.info(`Triggering Firebase event - ${this.firebaseAppId}`);
-      console.info(payload);
-    } else {
-      // Pino Logger for event logging with layload
+  async triggerEvent(payload: object) {
+    if (this.logger) {
       this.logger.info(`Triggering Firebase event - ${this.firebaseAppId}`);
       this.logger.child({ payload: payload });
+    } else {
+      console.error('Logger has not been set! Defaulting to "console.info"');
+      console.info(`Triggering Firebase event - ${this.firebaseAppId}`);
+      console.info(payload);
     }
 
-    return fetch(
+    return await fetch(
       `https://www.google-analytics.com/mp/collect?firebase_app_id=${this.firebaseAppId}&api_secret=${this.apiSecret}`,
       {
         method: "POST",
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,6 @@
+import type { Response } from "node-fetch";
+import type { Logger } from "pino";
+
 export class FirebaseEvents {
   /**
    * Constructor for FirebaseEvents class.
@@ -8,14 +11,14 @@ export class FirebaseEvents {
 
   /**
    *
-   * @param {unknown} logger - Provide a logger from PinoJS and branches (NestJS Pino, etc.)
+   * @param {Logger} logger - Provide a logger from PinoJS and branches (NestJS Pino, etc.)
    */
-  setLogger(logger: unknown): void;
+  setLogger(logger: Logger): void;
 
   /**
    * Method to add a new event or update an existing event.
    * @param {object} payload - An object which contains the JSON payload defined in the docs here: https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=firebase#payload_post_body
    * @returns {Promise} A response promise(fetch promise) from the provider, if successful, the response will be in JSON format. Errors need to be handled separately.
    */
-  triggerEvent(payload: object): Promise<unknown>;
+  triggerEvent(payload: object): Promise<Response>;
 }
